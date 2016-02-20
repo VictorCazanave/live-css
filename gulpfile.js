2c66abb3585964fafdc7804f9d856baa883126b9
Victor Cazanave <cazanave.v@sfeir.com>
2016-02-20 13:36:57 +0100

    [CONFIG] Create gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+	babel = require('gulp-babel'),
+	cssmin = require('gulp-cssmin'),
+	less = require('gulp-less'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	watch = require('gulp-watch'),
+
+	paths = {
+		src: './src',
+		dist: './dist',
+		js: ['./src/*.js'],
+		less: ['./src/*.less'],
+		css: ['./src/*.css']
+	};
+
+// Generate css files
+gulp.task('less', function () {
+	return gulp.src(paths.less)
+		.pipe(less())
+		.pipe(gulp.dest(paths.src));
+});
+
+// Watch less files
+gulp.task('watch', function () {
+	gulp.watch(paths.less, ['less']);
+});
+
+
+// Build js and css files
+gulp.task('build', ['less'], function () {
+	gulp.src(paths.js)
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.dist));
+
+	gulp.src(paths.css)
+		.pipe(cssmin())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(paths.dist));
+});
